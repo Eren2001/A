58a08bce349082238afa601e53bf4cc7b835e1d4
Eren2001 <79048894+Eren2001@users.noreply.github.com>
2021-02-16 19:58:17 +0300

    A
    A

---
+++ 100 sayı önce tek sonra çift.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int s[100], i, x;
+	for (i = 0; i <= 99; i++) {
+		cout << i + 1 << ".sayi = ";
+		cin >> x;
+		s[i] = x;
+	}
+	cout << "Dizinin tek elemanlari";
+	for (i = 0; i <= 99; i++) {
+		if (s[i] % 2 == 0) {
+
+		}
+		else {
+			cout <<"\n"<<s[i]<<"\n";
+		}
+	}
+	cout << "Dizinin cift elemanlari";
+	for (i = 0; i <= 99; i++) {
+		if (s[i] % 2 == 0) {
+			cout <<"\n"<<s[i]<<"\n";
+		}
+	}
+}

0d90dddd2d64d57512ccb395b60c1d9626eeabe6
Eren2001 <79048894+Eren2001@users.noreply.github.com>
2021-02-16 23:26:56 +0300

    A
    A

---
+++ a b dizisinin ortalaması.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, s, i;
+    cout << "kaç elemanlı = ";
+    cin >> n;
+    int a[n], b[n], c[n];
+    for (i = 0; i <= n - 1; i++) {
+        cout << n - 1 + 1 << ".elemanı giriniz = ";
+        cin >> s;
+        a[i] = s;
+    }
+    for (i = 0; i <= n - 1; i++) {
+        cout << n - 1 + 1 << ".elemanı giriniz = ";
+        cin >> s;
+        b[i] = s;
+    }
+    for (i = 0; i <= n - 1; i++) {
+        c[i] = a[i] + b[i];
+        cout << c[i];
+    }
+}

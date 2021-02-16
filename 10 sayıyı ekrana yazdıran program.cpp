d388c90a9f25f4cdaa4756ac8cb035624266659c
Eren2001 <79048894+Eren2001@users.noreply.github.com>
2021-02-16 19:34:51 +0300

    A
    A

---
+++ 10 sayıyı ekrana yazdıran program.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int Sayı[10], d, s;
+	for (d = 0; d <= 9; d++) {
+		cout << d + 1 << ".sayi = ";
+		cin >> s;
+		Sayı[d] = s;
+	}
+	for (d = 0; d <= 9; d++) {
+		cout << Sayı[d];
+	}
+}

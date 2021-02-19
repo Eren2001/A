47ecdbac3074ac119b2cb0f5b6ccc370688f26dc
Eren2001 <79048894+Eren2001@users.noreply.github.com>
2021-02-19 12:29:59 +0300

    A
    A

---
+++ Tersten yazdıran program.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int A[10], B[10], s, i;
+	for (i = 0; i <= 9; i++) {
+		cout << i + 1 << ".sayi = ";
+		cin >> s;
+		A[i] = s;
+	}
+	for (i = 0; i <= 9; i++) {
+		B[i] = A[9 - i];
+		cout << B[i];
+	}
+}
